d9bbb004a21a4c89efb78f3b69ec2e6bb37a0d7d
Gajanan <gajananraut142@gmail.com>
2025-11-20 17:20:54 +0530

    in admin panal create admin.py file add field for table row

--- new_ecom/new_ecom_app/admin.py
+++ new_ecom/new_ecom_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import student
+from django.contrib import admin
 # Register your models here.
+# admin.site.register(student)
+
+class studentAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in student._meta.get_fields()]
+
+admin.site.register(student, studentAdmin)
